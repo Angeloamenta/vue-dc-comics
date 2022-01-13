4fe5d4c881996c16f0ff174794230e3a27c5afbb
Angeloamenta <angeloamenta7@gmail.com>
2022-01-13 15:11:55 +0100

    creazione header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>ciao</h1>
+</template>
+
+<script>
+export default {
+name: 'Header',
+  data() { 
+    return {
+      
+    }
+  }, 
+  created () {
+    console.log('created');
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
