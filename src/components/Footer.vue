5d8fdeb8ba1a2fac2caf5f1c25b8a34a1a64fad5
Angeloamenta <angeloamenta7@gmail.com>
2022-01-13 17:37:20 +0100

    footer-top link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,62 @@
 <template>
   <footer>
-      <div class="container">
-          <div class="footer-top">
-              <h1>top</h1>
+      <div class="footer-top">
+          <div class="container">
+              <div class="col-top">
+                  <h3>DC COMICS</h3>
+                  <ul class="list-none">
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                  </ul>
+                  <h3>SHOP</h3>
+                  <ul class="list-none">
+                    <li><a href="">aaa</a></li>
+                    <li><a href="">aaa</a></li>
+                  </ul>
+              </div>
+              <div class="col-top">
+                  <h3>DC</h3>
+                  <ul class="list-none">
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                  </ul>
+              </div>
+              <div class="col-top">
+                  <h3>SITES</h3>
+                  <ul class="list-none">
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                      <li><a href="">aaa</a></li>
+                  </ul>
+              </div>
+          <div class="img-dc">
+              <img src="../assets/img/dc-logo-bg.png" alt="">
           </div>
-          <h2>bottom</h2>
-          <div class="footer-bottom">
-
           </div>
-      </div>
+      </div>  
   </footer>
 </template>
 
@@ -19,5 +67,26 @@ name: 'Footer'
 </script>
 
 <style lang="scss" scoped>
-
+.footer-top {
+    background-image: url(../assets/img/footer-bg.jpg);
+    // padding: 30px 0;
+    width: 100%;
+     height: 400px;
+    color: white;
+    overflow: hidden;
+    .container {
+        display: flex;
+        justify-content: space-between;
+       .img-dc {
+           width: 78%;
+           display: flex;
+           justify-content: end;
+           align-items: center;
+       }
+        .col-top {
+            width: 12%;
+            
+        }
+    }
+}
 </style>
